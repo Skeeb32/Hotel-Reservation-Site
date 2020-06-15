66b0a1873d06bdebb566c99f7d2c734dc477faf5
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-06-15 13:30:04 -0700

    Routes, BrowserRouter, Switch

--- resort/src/App.js
+++ resort/src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import './App.css';
 
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+
+import { Route, Switch } from 'react-router-dom';
+
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <>hello from app </>;
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:bed" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
