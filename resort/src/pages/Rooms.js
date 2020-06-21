4b9a668e2b111aa41f97d7dd1437a67f430f9ad6
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-06-21 10:36:08 -0700

    RoomContainer

--- resort/src/pages/Rooms.js
+++ resort/src/pages/Rooms.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import Banner from "../components/Banner"
-import Hero from '../components/Hero'
-import {Link} from 'react-router-dom'
+import Banner from '../components/Banner';
+import Hero from '../components/Hero';
+import { Link } from 'react-router-dom';
+import RoomContainer from '../components/RoomContainer';
 
 const Rooms = () => {
-  return <Hero hero="roomsHero">
-    <Banner title="our rooms">
-      <Link to="/" className="btn-primary">
-        return home 
-      </Link>
-    </Banner>
-  </Hero>
+  return (
+    <>
+    <Hero hero="roomsHero">
+      <Banner title="our rooms">
+        <Link to="/" className="btn-primary">
+          return home
+        </Link>
+      </Banner>
+    </Hero>
+    <RoomContainer />
+      
+    </>
+  );
 };
 
 export default Rooms;
