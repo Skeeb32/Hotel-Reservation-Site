6c25e33c45167de363ac31a9434c044e93c6881d
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-07-08 17:29:57 -0700

    Wednesday

--- resort/src/pages/Rooms.js
+++ resort/src/pages/Rooms.js
@@ -10,7 +10,7 @@ const Rooms = () => {
     <Hero hero="roomsHero">
       <Banner title="our rooms">
         <Link to="/" className="btn-primary">
-          return home Monday
+          return home Wednesday
         </Link>
       </Banner>
     </Hero>
