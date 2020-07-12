f260f1da95d6af133178b45ccfebe9db69125103
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-07-11 20:31:00 -0700

    RoomContainer Extra

--- resort/src/components/RoomContainer.js
+++ resort/src/components/RoomContainer.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import RoomsFilter from './RoomFilter'
-import RoomsList from './RoomList'
+import React from 'react';
+import RoomsFilter from './RoomFilter';
+import RoomsList from './RoomList';
+import { RoomConsumer } from '../context';
+import Loading from './Loading';
 
 export default function RoomContainer() {
-    return (
-        <div>
+  return (
+    <RoomConsumer>
+      {value => {
+        console.log(value);
+
+        return (
+          <div>
             hello from rooms container
-            <RoomsFilter/>
-            <RoomsList/>
-        </div>
-    )
+            <RoomsFilter />
+            <RoomsList />
+          </div>
+        );
+      }}
+    </RoomConsumer>
+  );
 }
